6cb8e3371e17c2bb8378611a63a13d54460acb7b
sharan036 <sharangopal36@gmail.com>
2026-07-29 08:29:46 +0530

    Updates

---
+++ _next/static/g_foyodT8_D-oEbRpRXRX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mindly-clone//_next/:path+",
+        "destination": "/mindly-clone/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

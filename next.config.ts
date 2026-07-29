2387d241a60a988432753665a05339b07751fbd9
sharan036 <sharangopal36@gmail.com>
2026-07-29 16:27:30 +0530

    Fix image optimization for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "framerusercontent.com" },
       { protocol: "https", hostname: "theroac.com" },
     ],
   },
+
   basePath: "/mindly-clone",
   assetPrefix: "/mindly-clone/",
 };

a20cde25cf24c542a8720191f2c64f1d9506423a
sharan036 <sharangopal36@gmail.com>
2026-07-29 16:30:59 +0530

    Fix GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,26 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
 
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: "https", hostname: "framerusercontent.com" },
-      { protocol: "https", hostname: "theroac.com" },
+      {
+        protocol: "https",
+        hostname: "framerusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "theroac.com",
+      },
     ],
   },
 
-  basePath: "/mindly-clone",
-  assetPrefix: "/mindly-clone/",
+  basePath: isProd ? "/mindly-clone" : "",
+  assetPrefix: isProd ? "/mindly-clone/" : "",
 };
 
 export default nextConfig;
